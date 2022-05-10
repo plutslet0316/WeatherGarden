cdbc8c4a3c4b516b598ecd054120f7ed0d846c46
pyosangrok <94944414+pyosangrok@users.noreply.github.com>
2022-05-10 16:22:44 +0900

    날씨 위치 권한
    permission 부분 오류 해결 못함
    코드 오류는 안 나오는데
    알림 팝업 창이 뜨지 않음

---
+++ WeatherGarden/app/src/main/java/com/example/weathergarden/GPS.java
@@ -0,0 +1,61 @@
+package com.example.weather_gps;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    int nCurrentPermission = 0;
+    static final int PERMISSIONS_REQUEST = 0x0000001;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        OnCheckPermission();
+    }
+
+    public void OnCheckPermission() {
+        Toast.makeText(this, "테스트", Toast.LENGTH_SHORT).show();
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED ||
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this, "테스트1", Toast.LENGTH_SHORT).show();
+//            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION))
+//            {
+            Toast.makeText(this, "앱 실행을 위해서는 권한을 설정해야 합니다", Toast.LENGTH_SHORT).show();
+
+            String[] a = new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION};
+            Toast.makeText(this, a[0], Toast.LENGTH_SHORT).show();
+
+            ActivityCompat.requestPermissions(this,
+                    a,
+                    PERMISSIONS_REQUEST);
+            //}
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "앱 실행을 위한 권한이 설정 되었습니다", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(this, "앱 실행을 위한 권한이 취소 되었습니다", Toast.LENGTH_LONG).show();
+                }
+                break;
+        }
+    }
+}
