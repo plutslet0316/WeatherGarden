705ba6104a171662225f024d0bd8806fcca2227a
plutslet0316 <94503803+plutslet0316@users.noreply.github.com>
2022-05-24 23:12:09 +0900

    기능추가: 날씨 가져오기
    스플레시 화면에서 위치를 먼저 가져오고
    날씨 화면에서 해당 위치를 반영하는 방식으로
    테스트 진행했습니다.
    
    더 좋은 방식이 있을 거 같은데 한계라서
    이 정도로 구현해보았습니다.

---
+++ WeatherGarden/app/src/main/java/com/example/weathergarden/weather/GPS.java
@@ -0,0 +1,179 @@
+package com.example.weathergarden.weather;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.lifecycle.ViewModel;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.Service;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.CancellationTokenSource;
+import com.google.android.gms.tasks.OnCanceledListener;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.gson.Gson;
+
+import java.util.HashMap;
+
+public class GPS  extends ViewModel {
+    Context context;
+
+    SharedPreferences preferences;
+    SharedPreferences.Editor editor;
+
+    FusedLocationProviderClient fusedLocationClient;
+
+    public boolean isGetLocation;
+
+    public GPS(Context context) {
+        this.context = context;
+
+        preferences = context.getSharedPreferences("player_data", Context.MODE_PRIVATE);
+        editor = preferences.edit();
+
+        isGetLocation = false;
+    }
+
+    private void setGridXY(Double x, Double y) {
+        try {
+            Double RE = 6371.00877;     // 지구 반경(km)
+            Double GRID = 5.0;          // 격자 간격(km)
+            Double SLAT1 = 30.0;        // 투영 위도1(degree)
+            Double SLAT2 = 60.0;        // 투영 위도2(degree)
+            Double OLON = 126.0;        // 기준점 경도(degree)
+            Double OLAT = 38.0;         // 기준점 위도(degree)
+            int XO = 43;             // 기준점 X좌표(GRID)
+            int YO = 136;            // 기준점 Y좌표(GRID)
+            Double DEGRAD = Math.PI / 180.0;
+            Double re = RE / GRID;
+            Double slat1 = SLAT1 * DEGRAD;
+            Double slat2 = SLAT2 * DEGRAD;
+            Double olon = OLON * DEGRAD;
+            Double olat = OLAT * DEGRAD;
+
+            Double sn = Math.tan(Math.PI * 0.25 + slat2 * 0.5) / Math.tan(Math.PI * 0.25 + slat1 * 0.5);
+            sn = Math.log(Math.cos(slat1) / Math.cos(slat2)) / Math.log(sn);
+            Double sf = Math.tan(Math.PI * 0.25 + slat1 * 0.5);
+            sf = Math.pow(sf, sn) * Math.cos(slat1) / sn;
+            Double ro = Math.tan(Math.PI * 0.25 + olat * 0.5);
+            ro = re * sf / Math.pow(ro, sn);
+
+            Double ra = Math.tan(Math.PI * 0.25 + (x) * DEGRAD * 0.5);
+            ra = re * sf / Math.pow(ra, sn);
+            Double theta = y * DEGRAD - olon;
+            if (theta > Math.PI) theta -= 2.0 * Math.PI;
+            if (theta < -Math.PI) theta += 2.0 * Math.PI;
+            theta *= sn;
+
+            int nx = (int) (ra * Math.sin(theta) + XO + 0.5);
+            int ny = (int) (ro - ra * Math.cos(theta) + YO + 0.5);
+
+            Gson gson = new Gson();
+            LocationData locationData = new LocationData(x, y, nx, ny);
+            String locationString = gson.toJson(locationData);
+            //Log.d("Gps", locationString);
+
+            editor.putString("location_data", locationString);
+            editor.apply();
+        }
+        finally {
+            isGetLocation = true;
+            Log.d("Gps", "좌표값 변환 완료" + isGetLocation);
+        }
+    }
+
+    public void findLocation() {
+        Log.d("Gps", "좌표값 가져오기 시작");
+
+        try {
+
+            int hasFineLocationPermission = ContextCompat.checkSelfPermission(context,
+                    Manifest.permission.ACCESS_FINE_LOCATION);
+            int hasCoarseLocationPermission = ContextCompat.checkSelfPermission(context,
+                    Manifest.permission.ACCESS_COARSE_LOCATION);
+
+
+            if (hasFineLocationPermission == PackageManager.PERMISSION_GRANTED &&
+                    hasCoarseLocationPermission == PackageManager.PERMISSION_GRANTED) {
+
+                ;
+            }
+
+            fusedLocationClient = LocationServices.getFusedLocationProviderClient(context);
+
+            CancellationTokenSource cts = new CancellationTokenSource();
+
+            fusedLocationClient.getCurrentLocation(LocationRequest.PRIORITY_HIGH_ACCURACY, cts.getToken())
+                    .addOnSuccessListener( new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            //Log.d("Gps", (location != null) + "");
+                            if (location != null) {
+                                setGridXY(location.getLatitude(), location.getLongitude());
+                            }
+                        }
+                    })
+                    .addOnCanceledListener(new OnCanceledListener() {
+                        @Override
+                        public void onCanceled() {
+                            Log.d("Gps", "취소됨");
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.d("Gps", "실패: " + e.getMessage());
+                        }
+                    })
+                    .addOnCompleteListener(new OnCompleteListener<Location>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Location> task) {
+                        }
+                    });
+
+/*
+            fusedLocationClient.getLastLocation()
+                    .addOnSuccessListener( new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            Log.d("Gps", (location != null) + "");
+                            if (location != null) {
+                                getGridXY(location.getLatitude(), location.getLongitude());
+                            }
+                        }
+                    })
+                    .addOnCanceledListener(new OnCanceledListener() {
+                        @Override
+                        public void onCanceled() {
+                            Log.d("Gps", "취소됨");
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.d("Gps", "실패: " + e.getMessage());
+                        }
+                    });
+ */
+        } catch (Exception e){
+            Log.d("Gps", e.getMessage());
+        }
+    }
+
+}
