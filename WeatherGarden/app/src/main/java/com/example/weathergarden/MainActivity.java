9c7b964d3b1df356b1e00dcac27da977aa8c6f4c
plutslet0316 <94503803+plutslet0316@users.noreply.github.com>
2022-05-31 14:23:01 +0900

    Squashed commit of the following:
    commit 4bc84980e997d8cab5fbb770d21982a4de55e80a
    Merge: cf65072 cc32426
    Author: plutslet0316 <94503803+plutslet0316@users.noreply.github.com>
    Date:   Thu May 26 12:13:37 2022 +0900
    
        Merge branch 'master' into dev/ssm
    
    commit cc3242624df0d25fd45506dbf6a6536f735f1136
    Author: plutslet0316 <94503803+plutslet0316@users.noreply.github.com>
    Date:   Tue May 24 23:12:09 2022 +0900
    
        기능추가: 날씨 가져오기
    
        스플레시 화면에서 위치를 먼저 가져오고
        날씨 화면에서 해당 위치를 반영하는 방식으로
        테스트 진행했습니다.
    
        더 좋은 방식이 있을 거 같은데 한계라서
        이 정도로 구현해보았습니다.
    
    commit cf650728adeb25b9f3f8f3b231ffeb94fd10e42c
    Merge: 744ff9e b0f4072
    Author: plutslet0316 <94503803+plutslet0316@users.noreply.github.com>
    Date:   Thu May 26 12:06:59 2022 +0900
    
        Merge branch 'master' into dev/ssm
    
    commit b0f40723920b919d7411d85d29e3fa8130066d11
    Author: plutslet0316 <94503803+plutslet0316@users.noreply.github.com>
    Date:   Tue May 24 23:12:09 2022 +0900
    
        기능추가: 날씨 가져오기
    
        스플레시 화면에서 위치를 먼저 가져오고
        날씨 화면에서 해당 위치를 반영하는 방식으로
        테스트 진행했습니다.
    
        더 좋은 방식이 있을 거 같은데 한계라서
        이 정도로 구현해보았습니다.
    
    commit 744ff9e616947214663a1dbbdda9deaf353ab057
    Author: suminseo21 <94975039+suminseo21@users.noreply.github.com>
    Date:   Tue May 24 15:41:37 2022 +0900
    
        dark버전 타이틀 바 수정
    
        app -> res -> values -> themes(night)
    
    commit b32688baec1d2e7c1ffca6e989fd328d649b72c2
    Author: suminseo21 <94975039+suminseo21@users.noreply.github.com>
    Date:   Tue May 24 15:37:05 2022 +0900
    
        하단메뉴 구현
    
        하단메뉴 클릭시 화면 이동 및 아이콘 색 변경 코드

--- WeatherGarden/app/src/main/java/com/example/weathergarden/MainActivity.java
+++ WeatherGarden/app/src/main/java/com/example/weathergarden/MainActivity.java
@@ -1,14 +1,72 @@
 package com.example.weathergarden;
 
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.LinearLayout;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    LinearLayout main;
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init(); // 객체 정의
+        SettingListener();
+
     }
+    private void init() {
+        main = findViewById(R.id.main);
+        bottomNavigationView = findViewById(R.id.bottom);
+    }
+    private void SettingListener() {
+        // 선택 리스너 등록
+        bottomNavigationView.setOnNavigationItemSelectedListener(new TabSelectedListener());
+    }
+
+    class TabSelectedListener implements BottomNavigationView.OnNavigationItemSelectedListener{
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId() ) {
+                case R.id.weather:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.main, new weatherFragment() )
+                            .commit();
+                    return true;
+            }
+            switch (item.getItemId() ) {
+                case R.id.garden:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.main, new gardenFragment() )
+                            .commit();
+                    return true;
+            }
+            switch (item.getItemId() ) {
+                case R.id.settings:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.main, new settingsFragment() )
+                            .commit();
+                    return true;
+            }
+            switch (item.getItemId() ) {
+                case R.id.notice:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.main, new noticeFragment() )
+                            .commit();
+                    return true;
+            }
+            switch (item.getItemId() ) {
+                case R.id.store:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.main, new storeFragment() )
+                            .commit();
+                    return true;
+            }
+            return false;
+        }
+    }
+
 }
