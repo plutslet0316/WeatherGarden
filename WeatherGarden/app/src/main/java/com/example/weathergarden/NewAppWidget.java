00cec0139c30548a97cbd2ab450264a46e4eb129
2jhl <94943448+2jhl@users.noreply.github.com>
2022-06-02 00:25:35 +0900

    기능추가: 위젯, 상단바 포그라운드 서비스
    위젯 추가한 후 새로고침 버튼으로 현재 시간 업데이트, 상단바 알림은 포그라운드 서비스로 고정되며 현재 날짜 표시
    
    오류수정: 위젯, 포그라운드 서비스 Android 12 실행 오류 해결
    
    Android 12 (S Emulator)에서 위젯&포그라운드 서비스가 실행되지 않는 문제 해결
    
    기능추가: 위젯, 상단바 포그라운드 서비스
    
    위젯 추가한 후 새로고침 버튼으로 현재 시간 업데이트, 상단바 알림은 포그라운드 서비스로 고정되며 현재 날짜 표시
    
    오류수정: 위젯, 포그라운드 서비스 Android 12 실행 오류 해결
    
    Android 12 (S Emulator)에서 위젯&포그라운드 서비스가 실행되지 않는 문제 해결
    
    지금까지 합쳐진 화면
    
    위젯 메인화면은 MainActivityWidget과 activity_main_widget으로 만들어 뒀습니다

---
+++ WeatherGarden/app/src/main/java/com/example/weathergarden/NewAppWidget.java
@@ -0,0 +1,126 @@
+package com.example.weathergarden;
+
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+
+import android.content.SharedPreferences;
+import android.widget.RemoteViews;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.weathergarden.weather.WeatherInfo;
+import com.example.weathergarden.weather.WeatherProc;
+import com.google.gson.Gson;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+
+public class NewAppWidget extends AppWidgetProvider {
+
+    private Fragment activity;
+
+    private static final String ACTION_BUTTON1 = "com.js.example.noti.BUTTON1";
+
+    void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                         int appWidgetId) {
+        // Construct the RemoteViews object
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.new_app_widget);
+
+        // 시작되면서 동적으로 타이틀 넣고 스타일 설정하기
+        //CharSequence widgetText = context.getString(R.string.appwidget_text);
+
+        //버튼1 클릭 : 클릭 성공 메세지 출력!
+        Intent intent1 = new Intent(ACTION_BUTTON1);
+        PendingIntent pendingIntent1 = PendingIntent.getBroadcast(context, 0, intent1, PendingIntent.FLAG_IMMUTABLE);
+        views.setOnClickPendingIntent(R.id.button1, pendingIntent1);
+
+        //Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.new_app_widget);
+        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+        ComponentName thisAppWidget = new ComponentName(context.getPackageName(), NewAppWidget.class.getName());
+        int[] appWidgets = appWidgetManager.getAppWidgetIds(thisAppWidget);
+
+        final String action = intent.getAction();
+        if (action.equals(ACTION_BUTTON1)) {
+            //your code here
+            Toast.makeText(context, "업데이트 완료", Toast.LENGTH_SHORT).show();
+            onUpdate(context, appWidgetManager, appWidgets);
+        }
+    }
+
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+
+            RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.new_app_widget);
+
+            remoteViews.setTextViewText(R.id.updatetime, "업데이트 " + formatDate());
+
+            // 날씨 적용
+            //WeatherProc weatherProc =new WeatherProc(this);
+            //weatherProc.getWeather();
+
+            //WeatherInfo weatherInfo = weatherProc.getWeatherInfo();
+
+            remoteViews.setTextViewText(R.id.temp, String.format("℃"));
+
+            //remoteViews.setTextViewText(R.id.temp, String.format("기온: %dº", WeatherInfo.temp));
+
+
+            //버튼1 클릭 : 클릭 성공 메세지 출력!
+            remoteViews.setOnClickPendingIntent(R.id.button1, getPendingSelfIntent(context, ACTION_BUTTON1, PendingIntent.FLAG_IMMUTABLE));
+
+            appWidgetManager.updateAppWidget(appWidgetId, remoteViews);
+        }
+    }
+
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+
+    protected PendingIntent getPendingSelfIntent(Context context, String action, int flag) {
+        Intent intent = new Intent(context, getClass());
+        intent.setAction(action);
+
+        return PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_IMMUTABLE);
+    }
+
+    public String formatDate() {
+        long mNow = System.currentTimeMillis(); // 현재 시간 가져옴
+        Date mReDate = new Date(mNow); // Date 형식 적용
+        SimpleDateFormat mFormat = new SimpleDateFormat("MM/dd HH:mm"); // 시간을 원하는 포맷으로 변경
+        String formatDate = mFormat.format(mReDate);
+
+        return formatDate;
+    }
+
+
+}
