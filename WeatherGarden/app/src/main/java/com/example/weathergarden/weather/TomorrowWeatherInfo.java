87ca8e12bf405e33b1ec28df15779ea583d70a3c
plutslet0316 <plutslet0316@gmail.com>
2022-06-06 19:44:46 +0900

    기능추가: 단기예보 가져오는 기능 추가

---
+++ WeatherGarden/app/src/main/java/com/example/weathergarden/weather/TomorrowWeatherInfo.java
@@ -0,0 +1,10 @@
+package com.example.weathergarden.weather;
+
+public class TomorrowWeatherInfo {
+    public String fcstDate;       // 예보시각
+    public String fcstTime;       // 예보시각
+    public String rainType;       // 강수 형태
+    public String temp;           // 기온
+    public String humidity;       // 습도
+    public String sky;            // 하늘 상태
+}
