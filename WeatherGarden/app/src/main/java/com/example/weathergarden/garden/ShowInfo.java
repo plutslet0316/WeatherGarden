88b81977ad5d4b462f3747975ba2a5c3c23e81ea
plutslet0316 <plutslet0316@gmail.com>
2022-09-28 01:24:20 +0900

    기능추가: 날씨와 온도를 저장하고 불러오는 기능 추가
    SharedPreferences를 이용해 시뮬레이션에 필요한
    날씨와 온도를 가져올 수 있는 기능을 추가했습니다.

---
+++ WeatherGarden/app/src/main/java/com/example/weathergarden/garden/ShowInfo.java
@@ -0,0 +1,11 @@
+package com.example.weathergarden.garden;
+
+public class ShowInfo {
+    public String weather;
+    public String temp;
+
+    public void init(){
+        weather = "0";
+        temp = "25.0";
+    }
+}
