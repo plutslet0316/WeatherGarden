3cc7dbfc76f62425962ff2cdac8351201c82729b
2jhl <94943448+2jhl@users.noreply.github.com>
2022-10-03 15:46:39 +0900

    기능추가: 식물 데이터베이스 추가
    SQLite 이용한 식물 데이터베이스
    assets 파일에 weather.db 파일 추가

--- WeatherGarden/app/src/main/java/com/example/weathergarden/MainActivity.java
+++ WeatherGarden/app/src/main/java/com/example/weathergarden/MainActivity.java
@@ -1,14 +1,49 @@
 package com.example.weathergarden;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.example.weathergarden.SQLite.DataBaseHelper;
 
 public class MainActivity extends AppCompatActivity {
+
+    float val = 0;
+
+    Button btnSelect;
+    TextView plant_book, title, content;
+    //  "dbWeaGar.db", // DB 파일 이름
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("식물도감 DB");
+
+        getVal();
+
+        btnSelect = (Button) findViewById(R.id.btnSelect);
+        plant_book = (TextView) findViewById(R.id.plant_book);
+        title = (TextView) findViewById(R.id.title);
+        content = (TextView) findViewById(R.id.content);
+    }
+
+    public void getVal() {
+
+        DataBaseHelper dbHelper = new DataBaseHelper(this);
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+
+        Cursor cursor = db.rawQuery("SELECT * FROM plants where plant_code = 1", null);
+        if (cursor.moveToNext())
+        {
+            val = cursor.getFloat(3);
+        }
+
+        cursor.close();
+        dbHelper.close();
     }
 }
